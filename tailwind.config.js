f1bc95c2744575480339ba2e76f59e95ecf667db
Nicholas <nicholasikiroma@gmail.com>
2023-05-01 14:16:19 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,55 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./static/src/**/*.js",
+    "./node_modules/flowbite/**/*.js"
+  ],
+    darkMode: 'class',
+    theme: {
+      extend: {
+        colors: {
+          primary: {"50":"#faf5ff","100":"#f3e8ff","200":"#e9d5ff","300":"#d8b4fe","400":"#c084fc","500":"#a855f7","600":"#9333ea","700":"#7e22ce","800":"#6b21a8","900":"#581c87"}
+        }
+      },
+      fontFamily: {
+        'body': [
+      'Source Sans Pro', 
+      'ui-sans-serif', 
+      'system-ui', 
+      '-apple-system', 
+      'system-ui', 
+      'Segoe UI', 
+      'Roboto', 
+      'Helvetica Neue', 
+      'Arial', 
+      'Noto Sans', 
+      'sans-serif', 
+      'Apple Color Emoji', 
+      'Segoe UI Emoji', 
+      'Segoe UI Symbol', 
+      'Noto Color Emoji'
+    ],
+        'sans': [
+      'Source Sans Pro', 
+      'ui-sans-serif', 
+      'system-ui', 
+      '-apple-system', 
+      'system-ui', 
+      'Segoe UI', 
+      'Roboto', 
+      'Helvetica Neue', 
+      'Arial', 
+      'Noto Sans', 
+      'sans-serif', 
+      'Apple Color Emoji', 
+      'Segoe UI Emoji', 
+      'Segoe UI Symbol', 
+      'Noto Color Emoji'
+    ]
+      }
+    },
+  plugins: [
+    require("flowbite/plugin")
+  ],
+}
